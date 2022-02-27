98e6190e24cad0f4a4d791b0f457c0de6680e58b
Gabriel Lopes <lopesgabriel0199@gmail.com>
2022-02-27 16:19:22 -0300

    delete: Removes hello lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,6 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
-import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { join } from 'path'
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
@@ -14,20 +13,10 @@ export class SpaceStack extends Stack {
   constructor (scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
 
-    const helloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main'
-    })
-
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler'
     })
-
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda)
-    const helloLambdaResource = this.api.root.addResource('hello')
-    helloLambdaResource.addMethod('Get', helloLambdaIntegration)
 
     const helloLambdaNodeJsIntegration = new LambdaIntegration(helloLambdaNodeJs)
     const helloLambdaNodeJsResource = this.api.root.addResource('helloNodeJs')

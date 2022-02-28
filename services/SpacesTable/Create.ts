3440fb7aab96fe5ae028af3349da726afbe4e003
Gabriel Lopes <lopesgabriel0199@gmail.com>
2022-02-28 15:08:43 -0300

    feat: Adds put item function

---
+++ services/SpacesTable/Create.ts
@@ -0,0 +1,34 @@
+import { v4 } from 'uuid'
+import { DynamoDB } from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+
+const dbClient = new DynamoDB.DocumentClient();
+
+export const handler = async (event: APIGatewayProxyEvent, _context: Context): Promise<APIGatewayProxyResult> => {
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: 'Hello from DynamoDB'
+  }
+
+  const item = typeof event.body === 'object'
+    ? event.body
+    : JSON.parse(event.body)
+
+  item.spaceId = v4()
+
+  try {
+    const operationResult = await dbClient.put({
+      TableName: 'SpacesTable',
+      Item: item
+    }).promise()
+
+    if (!operationResult.$response.error) {
+      result.body = 'Successfuly created a item with id ' + item.spaceId
+    }
+  } catch(err: any) {
+    result.body = err.message
+    result.statusCode = 500
+  }
+
+  return result
+}
